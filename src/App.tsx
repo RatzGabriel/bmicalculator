dd79d87860d6bee73df86b2865e8a1c01f0bcc8c
gabriel <gabrielratz84@gmai.com>
2021-04-26 09:14:44 +0200

    set onChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  const onChange = (val: number, name: string) => {
+    if (val >= 0 && val < 5000) {
+      if (name === "weight") {
+        console.log(val);
+
+        setWeight(val);
+      } else if (name === "height") {
+        setHeight(val);
+      } else return;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="">
+        <input
+          onChange={(e) => onChange(Number(e.target.value), e.target.name)}
+          name="weight"
+          type="text"
+          pattern="[0-9]"
+          placeholder="Weight in kg"
+          value={weight}
+        />
+        <input
+          onChange={(e) => onChange(Number(e.target.value), e.target.name)}
+          name="height"
+          type="number"
+          placeholder="Height in kg"
+          pattern="[0-9]"
+          value={height}
+        />
+        <button>Calculate BMI</button>
+      </form>
+      <button onClick={() => console.log(weight)}>Test</button>
     </div>
   );
 }
